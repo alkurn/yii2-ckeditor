7e983298358b03979adc4751737c53a6c05341b7
Ganesh <ganesh.alkurn@gmail.com>
2017-11-13 11:01:22 +0530

    update

--- src/presets/minimum.php
+++ src/presets/minimum.php
@@ -12,8 +12,6 @@ return [
     'toolbarGroups' => [
         ['name' => 'basicstyles', 'groups' => ['basicstyles', 'cleanup']],
         ['name' => 'colors'],
-        ['name' => 'links', 'groups' => ['links', 'insert']],
-        ['name' => 'others', 'groups' => ['others', 'about']],
     ],
     'removeButtons' => 'Subscript,Superscript,Flash,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe',
     'removePlugins' => 'elementspath',
